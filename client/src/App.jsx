e66525a8400ffac290b859da9d25f73d2d7b4657
Suyash Shrestha <sthasuyash11@gmail.com>
2023-03-29 21:53:14 +0545

    update client structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
-import './assets/Styles/App.scss'
+import './App.scss'
 
 function App() {
   return (
     // create a simple hello world text
     <div className="app">
-      <h1 className="title">Hello React</h1>
+      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">This is new React</h1>
     </div>
   )
 }
