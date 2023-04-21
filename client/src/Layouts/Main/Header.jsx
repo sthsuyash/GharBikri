ce416acf96a142c74538bcb5fe861131eb1e3db6
Suyash Shrestha <sthasuyash11@gmail.com>
2023-04-22 01:28:51 +0545

    update error page

--- client/src/Layouts/Main/Header.jsx
+++ client/src/Layouts/Main/Header.jsx
@@ -35,7 +35,7 @@ function Header() {
                         <img src="https://images.unsplash.com/photo-1510227272981-87123e259b17?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=3759e09a5b9fbe53088b23c615b6312e" className="w-10 h-10 rounded-full border-2 border-white" />
                         <img src="https://randomuser.me/api/portraits/men/65.jpg" className="w-10 h-10 rounded-full border-2 border-white" />
                         <p className=" text-gray-500 text-md translate-x-5">
-                            Join our 5.000+ family
+                            Our 5.000+ family
                         </p>
                     </div>
                 </div>
