c625ecfe7d2c219015e7e370aac39483a0bd82dc
prashannastha7 <prashannabdrshrestha711@gmail.com>
2023-04-22 22:46:02 +0545

    update query

--- server/src/controllers/rentController.js
+++ server/src/controllers/rentController.js
@@ -2,12 +2,11 @@ const db = require("../db");
 
 exports.getRent = async (req, res) => {
     try{
-        const user = await db.query("SELECT user_id, username, user_email FROM users WHERE user_id = 'e8cff7c5-b774-4d69-89d9-0d88dd32dc40'");
+        const user = await db.query("SELECT user_id, username, user_email FROM users WHERE user_id = $1", [req.user]);
         // your rent logic goes here
          res.status(200).json({
             success: true,
             message: 'Rent successful',
-            user: user
         });
     } catch (error) {
         console.log(error.message);
