5d2fdd4ca97bb145f2b3c3cf4ea47168715ef360
prashannastha7 <prashannabdrshrestha711@gmail.com>
2023-04-23 19:21:29 +0545

    buyController

---
+++ server/src/controllers/buyController.js
@@ -0,0 +1,19 @@
+const db = require("../db");
+
+exports.getBuy = async (req, res) => {
+    try{
+        const user = await db.query("SELECT user_id, username, user_email FROM users WHERE user_id = $1", [req.user]);
+        // your rent logic goes here
+         res.status(200).json({
+            success: true,
+            message: 'Rent successful',
+            //user: user
+        });
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+}
