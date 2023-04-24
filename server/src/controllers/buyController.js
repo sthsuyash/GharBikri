615121d5d767c5374526230c3b890fe7f3915368
Suyash Shrestha <sthasuyash11@gmail.com>
2023-04-24 20:25:42 +0545

    update buy backend

--- server/src/controllers/buyController.js
+++ server/src/controllers/buyController.js
@@ -1,20 +1,41 @@
 // buy controller
 
-// const { Property } = require('../models/Property');
+const db = require('../db');
 
 // get all properties which have status of buy
 const getBuy = async (req, res) => {
     try {
-        // const properties = await Property.find({ status: 'buy' });
-        // res.status(200).json({ properties });
-        console.log('getBuy');
+        const properties = await db.query("SELECT * FROM properties WHERE status = $1", ["buy"]);
+        res.status(200).json({ properties: properties.rows });
+    } catch (error) {
+        res.status(500).json({ error });
+    }
+}
+
+// get all properties which have status of buy and filter according to the price range, city, and type
+const getBuyFilter = async (req, res) => {
+    try {
+        const { price, city, type } = req.query;
+        const properties = await db.query("SELECT * FROM properties WHERE status = $1 AND price BETWEEN $2 AND $3 AND city = $4 AND type = $5", ["buy", price[0], price[1], city, type]);
+        res.status(200).json({ properties: properties.rows });
+    } catch (error) {
+        res.status(500).json({ error });
+    }
+}
+
+// get individual property after clicking on it
+const getBuyId = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const property = await db.query("SELECT * FROM properties WHERE property_id = $1", [id]);
+        res.status(200).json({ property: property.rows[0] });
     } catch (error) {
         res.status(500).json({ error });
     }
 }
 
 module.exports = {
-    getBuy
+    getBuy,
+    getBuyFilter,
+    getBuyId
 }
-
-
