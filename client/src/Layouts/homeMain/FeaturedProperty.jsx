8abac258586c59d275417af864bccdbcd878859d
Suyash Shrestha <sthasuyash11@gmail.com>
2023-05-05 01:27:16 +0545

    update featured property

--- client/src/Layouts/homeMain/FeaturedProperty.jsx
+++ client/src/Layouts/homeMain/FeaturedProperty.jsx
@@ -64,24 +64,26 @@ export default function FeaturedProperty() {
                     <h2 className="mb-3 font-semibold lg:text-[48px] text-3xl text-center lg:text-left">Featured Property</h2>
                     <p className="text-center lg:text-left text-gray-400 sm:mb-12 mb-10">Let&apos;s find you a comfortable place</p>
 
-                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-16 lg:mx-0 mx-8">
-                      
+                    <h3 className="text-center lg:text-left text-3xl font-semibold mb-8">Rent</h3>
+                    {rentProperties ? (
+                        <>
+                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-16 lg:mx-0 mx-8">
+                                {
+                                    rentProperties.map((property) => (
+                                        <PropertyCard key={property.p_id} property={property} />
+                                    ))
+                                }
+                            </div>
 
-                        {rentProperties ?
-                            rentProperties.map((property) => (
-                                <PropertyCard key={property.p_id} property={property} />
-                            )
-                            ) : (
-                                <div className="text-center text-2xl font-bold text-gray-400">No properties found</div>
-                            )
-                        }
-                    </div>
-
-                    <div className="mt-12 text-center outline-none block text-sm text-gray-600">
-                        <Link className="h-10 rounded-3xl text-sm shadow-md bg-none p-4 hover:bg-black hover:text-white transition-all" to="/properties">
-                            View all
-                        </Link>
-                    </div>
+                            <div className="my-12 text-center outline-none block text-sm text-gray-600">
+                                <Link className="h-10 rounded-3xl text-sm shadow-md bg-none p-4 hover:bg-black hover:text-white transition-all" to="/rent">
+                                    View all
+                                </Link>
+                            </div>
+                        </>
+                    ) : (
+                        <div className="text-center text-2xl font-bold text-gray-400">No properties found</div>)
+                    }
 
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                         {buyProperties ?
